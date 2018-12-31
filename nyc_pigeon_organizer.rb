82d8d9448f0e66f10aed1edee7434c75f5ea7686
Raymond Chow <raychow1944@yahoo.com>
2018-12-31 05:28:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, values|
+    values.each do |value, names|
+      names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+          pigeon_list[name][attribute] = []
+          pigeon_list[name][attribute] << value.to_s
+        elsif pigeon_list[name] != nil
+          if pigeon_list[name][attribute] == nil
+            pigeon_list[name][attribute] = []
+          end
+          pigeon_list[name][attribute] << value.to_s
+        end
+      end
+      #binding.pry
+    end
+  end
+  pigeon_list
 end
